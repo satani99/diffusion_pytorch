7314983c98d5064cd41249a3cbeb260044d5d929
satani99 <sataninikhil@gmail.com>
2024-11-28 20:41:46 +0530

    added edecoder

--- sd/decoder.py
+++ sd/decoder.py
@@ -1,2 +1,44 @@
 import torch
+from torch import nn
+from torch.nn import functional as F
+from attention import SelfAttention 
 
+class VAE_ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.groupnorm_1 = nn.GroupNorm(32, in_channels)
+        self.conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+
+        self.groupnorm_2 = nn.GroupNorm(32, out_channels)
+        self.conv_2 = nn.Conv2d(out_channels, out_channels, kernel_size=1, padding=1)
+        
+        if in_channels == out_channels:
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
+
+    def forward(self, x):
+        # x: (Batch_Size, In_Channels, Height, Width)
+
+        residue = x 
+
+        # (Batch_Size, In_Channels, Height, Width) -> (Batch_Size, In_Channels, Height, Width)
+        x = self.groupnorm_1(x)
+
+        # (Batch_Size, Out_Channels, Height, Width) -> (Batch_Size, In_Channels, Height, Width)
+        x = F.silu(x)
+
+        # (Batch_Size, In_Channels, Height, Width) -> (Batch_Size, Out_Channels, Height, Width)
+        x = self.conv_1(x)
+
+        # (Batch_Size, Out_Channels, Height, Width) -> (Batch_Size, Out_Channels, Height, Width)
+        x = self.groupnorm_2(x)
+
+        # (Batch_Size, Out_Channels, Height, Width) -> (Batch_Size, Out_Channels, Height, Width)
+        x = F.silu(x)
+
+        # (Batch_Size, Out_Channels, Height, Width) -> (Batch_Size, Out_Channels, Height, Width)
+        x = self.conv_2(x)
+
+        # (Batch_Size, Out_Channels, Height, Width) -> (Batch_Size, Out_Channels, Height, Width)
+        return x + self.residual_layer(residue)
--- sd/encoder.py
+++ sd/encoder.py
@@ -94,4 +94,40 @@ class VAE_Encoder(nn.Module):
         
         return x
 
-        
+class VAE_AttentionBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.groupnorm = nn.GroupNorm(32, channels)
+        self.attention = SelfAttention(1, channels)
+
+    def forward(self, x):
+        # x: (Batch_Size, Channels, Height, Width)
+
+        residue = x 
+
+        # (Batch_Size, Channels, Height, Width) -> (Batch_Size, Channels, Height, Width)
+        x = self.groupnorm(x)
+
+        n, c, h, w = x.shape
+
+        # (Batch_Size, Channels, Height, Width) -> (Batch_Size, Channels, Height * Width)
+        x = x.view((n, c, h * w))
+
+        # (Batch_Size, Channels, Height * Width) -> (Batch_Size, Height * Width, Channels) 
+        x = x.transpose(-1, -2)
+
+        # Perform self-attention WITHOUT mask 
+        # (Batch_Size, Height * Width, Channels) -> (Batch_Size, Height * Width, Channels) 
+        x = self.attention(x)
+
+        # (Batch_Size, Height * Width, Channels) -> (Batch_Size, Channels, Height * Width) 
+        x = x.transpose(-1, -2)
+
+        # (Batch_Size, Channels, Height * Width) -> (Batch_Size, Channels, Height, Width)
+        x = x.view((n, c, h, w))
+
+        # (Batch_Size, Channels, Height, Width) -> (Batch_Size, Channels, Height, Width)
+        x += residue
+
+        # (Batch_Size, Channels, Height, Width)
+        return x
